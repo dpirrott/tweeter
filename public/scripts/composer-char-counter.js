7d1737ea7be0a256181a055966ef9507da479ae8
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-02-28 22:39:42 -0800

    Implement char counter js file, update profile picture

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
   
-  $('#tweet-text').on('input', function() => {
+  $('#tweet-text').on('input', function() {
     console.log(this.value.length);
+    const remainingLength = 140 - this.value.length;
+    $('.counter').text(remainingLength);
   })
 
 });
