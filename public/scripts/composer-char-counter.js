47830beea2b7a4b3c701ff35b278177203a3176e
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-02-28 22:58:42 -0800

    Add conditional colour logic to counter text

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,16 @@ $(document).ready(function() {
     console.log(this.value.length);
     const remainingLength = 140 - this.value.length;
     $('.counter').text(remainingLength);
-  })
+    if (remainingLength < 0) {
+      $('.counter').css('color', 'red');
+      $('.counter').css('background-color', 'inherit');
+    } else if (remainingLength < 6) {
+      $('.counter').css('background-color', 'yellow');
+      $('.counter').css('color', 'black');
+    } else {
+      $('.counter').css('background-color', 'inherit');
+      $('.counter').css('color', 'black');
+    }
+  });
 
 });
