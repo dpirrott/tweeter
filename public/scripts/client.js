345f0c8cc1f6fbe002d7a5db7959d3f115e97e77
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-01 22:26:04 -0800

    Implement render tweets function, remove hardcoded data in index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,16 +6,36 @@
 
 $(() => {
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
   }
 
   const createTweetElement = function(tweetData) {
@@ -39,9 +59,6 @@ $(() => {
     `)
   };
 
-  const $tweet = createTweetElement(tweetData);
-
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
 });
 
