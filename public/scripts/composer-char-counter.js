3feb08b68f8e351be3f9c0f75d05352295d4c5e1
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-01 18:07:37 -0800

    Update char counter js, add/remove css classes instead of direct css setting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,15 @@
-$(document).ready(function() {
+$(() => {
   
   $('#tweet-text').on('input', function() {
     const remainingLength = 140 - this.value.length;
-    $('.counter').text(remainingLength);
+    const counter = $(this).next().find('output');
+    counter.text(remainingLength);
+    counter.removeClass();
+    counter.addClass('char-count');
     if (remainingLength < 0) {
-      $('.counter').css({'color': 'red', 'background-color': 'inherit'});
+      counter.addClass('char-count-overflowed');
     } else if (remainingLength < 6) {
-      $('.counter').css({'color': 'black', 'background-color': 'yellow'});
-    } else {
-      $('.counter').css({'color': 'black', 'background-color': 'inherit'});
+      counter.addClass('char-count-overflow-warning');
     }
   });
 
