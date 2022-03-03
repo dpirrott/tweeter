848a3b896015aa78f543bfedf5e60c7c0ff0918f
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-02 22:03:44 -0800

    Hide navbar/header when scrolling away from top

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -3,17 +3,27 @@ $(() => {
   $(window).scroll(() => {
     const scrollPosition = $(this).scrollTop();
     const scrollUpBtn = $('#scrollUpBtn');
+    const nav = $('nav');
+    const header = $('body').children('header');
     if (scrollPosition !== 0) {
       // Show scroll button if still hidden
       if (scrollUpBtn.hasClass("scroll-up-hide")) {
         scrollUpBtn.removeClass();
         scrollUpBtn.addClass("scroll-up-show");
       }
-      
 
+      // Hide the nav, somehow make tweeter heading stay
+      if (!nav.is(":hidden") || !header.is(":hidden")) {
+        nav.hide("slide");
+        header.hide("slide");
+      }
+      
     } else {
       scrollUpBtn.removeClass();
       scrollUpBtn.addClass("scroll-up-hide");
+
+      nav.slideDown()
+      header.slideDown()
     }
   });
 
