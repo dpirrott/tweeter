4dfeee0e2a3c71e932241c789820873e399c8c4b
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-03-02 21:33:38 -0800

    Create classes for new scroll up button states

--- public/scripts/composer.js
+++ public/scripts/composer.js
@@ -1,7 +1,18 @@
 $(() => {
 
   $(window).scroll(() => {
-    console.log($(this).scrollTop())
+    const scrollPosition = $(this).scrollTop()
+    if (scrollPosition !== 0) {
+      console.log("I'm not at top!")
+
+      // Add new button and collapse nav
+
+      // Add new button
+      
+
+    } else {
+      console.log("I'm at top!")
+    }
   });
 
 
